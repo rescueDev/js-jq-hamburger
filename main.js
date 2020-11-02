87b799c8a6b69f7783d51df4a2bdf0d184f94895
Salvatore Borgia <sa.borgia3@gmail.com>
2020-11-02 18:25:50 +0100

    aggiunta menu che si chiude quando da 1000px ritorno indietro

--- main.js
+++ main.js
@@ -19,3 +19,13 @@ function clickShow() {
 function clickHide() {
   submenu.removeClass("active");
 }
+
+//se allargo la finestra oltre i 1000px e ritorno indietro trovo il menu ancora aperto
+
+//uso resize per rimuovere la classe active al di sopra dei 1000px di larghezza
+
+$(window).resize(function () {
+  if ($(window).width() > 1000) {
+    submenu.removeClass("active");
+  }
+});
